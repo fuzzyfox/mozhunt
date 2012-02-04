cbaa77bf98debc7096c0a51ae4fcda6e7b83f0ca
Uruwolf <uru@mozhunt.com>
2012-02-04 22:08:47 +0000

    Quick framework finished

---
+++ application/controllers/userAdmin.php
@@ -0,0 +1,33 @@
+<?php
+// ./application/controllers/userAdmin.php
+
+/**
+ * Contains the methods for adminstering users
+ * @author Steve "Uru" West <sw349@kent.ac.uk>
+ * @version 2012-02-04
+ */
+class UserAdmin extends CI_Controller
+{
+	/**
+	 * Shows a list of users
+	 * @suthor Steve "Uru" West
+	 * @version 2012-02-04
+	 */
+	public function index()
+	{
+	}
+
+	public function remove()
+	{
+	}
+
+	public function add()
+	{
+	}
+
+	public function edit()
+	{
+	}
+}
+
+// End file file userAdmin.php
