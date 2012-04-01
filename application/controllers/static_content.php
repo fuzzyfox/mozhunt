d8877183fde46fcbede34e90ad81fbe2e10ec278
William Duyck <wduyck@gmail.com>
2012-04-01 13:31:51 +0100

    fixed a small bug with current homepage caused due to timezones

--- application/controllers/static_content.php
+++ application/controllers/static_content.php
@@ -12,9 +12,8 @@ class Static_content extends CI_Controller
 	{
 		parent::__construct();
 		
-		$this->load->library('form_validation');
-		$this->load->library('email');
-		$this->load->library('theme');
+		$this->load->library(array('form_validation', 'email', 'theme'));
+		$this->load->helper('date');
 	}
 	
 	/**
