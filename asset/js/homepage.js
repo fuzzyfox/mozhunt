264508f881c439ebc3390815fc652e24f3864212
William Duyck <wduyck@gmail.com>
2012-04-01 06:47:29 +0100

    fix for small error in homepage js. js date object dates start at 0 not 1

--- asset/js/homepage.js
+++ asset/js/homepage.js
@@ -1,8 +1,8 @@
 
 	setInterval(function(){
 		var currentTime = new Date();
-		var startTime = new Date(2012, 3, 1, 0, 0, 0, 0);
-		var finishTime = new Date(2012, 3, 4, 0, 0, 0, 0);
+		var startTime = new Date(2012, 3, 0, 0, 0, 0, 0);
+		var finishTime = new Date(2012, 3, 3, 0, 0, 0, 0);
 		var percentage = ((currentTime.getTime() - startTime.getTime()) / (finishTime.getTime() - startTime.getTime())) * 100;
 		console.log('Final Development Sprint ' + percentage);
 		$('#FinalDevelopmentSprint .bar').css('width', percentage + '%');
@@ -10,8 +10,8 @@
 	
 	setInterval(function(){
 		var currentTime = new Date();
-		var startTime = new Date(2012, 3, 4, 0, 0, 0, 0);
-		var finishTime = new Date(2012, 3, 8, 0, 0, 0, 0);
+		var startTime = new Date(2012, 3, 3, 0, 0, 0, 0);
+		var finishTime = new Date(2012, 3, 7, 0, 0, 0, 0);
 		var percentage = ((currentTime.getTime() - startTime.getTime()) / (finishTime.getTime() - startTime.getTime())) * 100;
 		console.log('Soft Launch ' + percentage);
 		$('#SoftLaunch .bar').css('width', percentage + '%');
