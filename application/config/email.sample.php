baa38b0a07502f00716022882c703461e07489a6
William Duyck <wduyck@gmail.com>
2012-04-06 15:01:17 +0100

    added some email config for sending mail

---
+++ application/config/email.sample.php
@@ -0,0 +1,15 @@
+<?php
+	
+	/**
+	 * This file contains the needed configuration for sending emails as
+	 * noreply@mozhunt.com
+	 *
+	 * @author William Duyck <william@mozhunt.com>
+	 * @version 2012.04.06
+	 */
+	
+	$config['protocol'] = 'smtp';
+	$config['smtp_host'] = '';
+	$config['smtp_user'] = '';
+	$config['smtp_pass'] = '';
+	$config['smtp_port'] = '';
