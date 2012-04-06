738e1024de5518b57a4dcfdc00add99167bb03c7
William Duyck <wduyck@gmail.com>
2012-04-06 01:43:27 +0100

    .

--- application/views/api_proxy.php
+++ application/views/api_proxy.php
@@ -62,7 +62,7 @@ var XD = function () {
 
 $mozhunt = {
 	send : function(msg){
-		var parentURL = decodeURIComponent(document.location.hash.replace(/^#/, ''));
+		var parent_url = decodeURIComponent(document.location.hash.replace(/^#/, ''));
 		console.log(parent);
 		XD.postMessage(JSON.stringify(msg), parent_url, parent);
 	}
