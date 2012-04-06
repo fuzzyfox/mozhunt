99c01cd99df919b2b2f4e8b927819a5d49c44459
William Duyck <wduyck@gmail.com>
2012-04-06 01:41:32 +0100

    .

--- asset/js/mozhuntclient.js
+++ asset/js/mozhuntclient.js
@@ -5,63 +5,6 @@
  * @author William Duyck <william@mozhunt.com>
  * @version 2012.04.01
  */
-$mozhuntpostmessage = function () {
-	var interval_id, last_hash, cache_bust = 1,
-		attached_callback, window = this;
-	return {
-		postMessage: function (message, target_url, target) {
-			if (!target_url) {
-				return;
-			}
-			target = target || parent; // default to parent
-			if (window['postMessage']) {
-				// the browser supports window.postMessage, so call it with a targetOrigin
-				// set appropriately, based on the target_url parameter.
-				target['postMessage'](message, target_url.replace(/([^:]+:\/\/[^\/]+).*/, '$1'));
-			} else if (target_url) {
-				// the browser does not support window.postMessage, so set the location
-				// of the target to target_url#message. A bit ugly, but it works! A cache
-				// bust parameter is added to ensure that repeat messages trigger the callback.
-				target.location = target_url.replace(/#.*$/, '') + '#' + (+new Date) + (cache_bust++) + '&' + message;
-			}
-		},
-		receiveMessage: function (callback, source_origin) {
-			// browser supports window.postMessage
-			if (window['postMessage']) {
-				// bind the callback to the actual event associated with window.postMessage
-				if (callback) {
-					attached_callback = function (e) {
-						if ((typeof source_origin === 'string' && e.origin !== source_origin) || (Object.prototype.toString.call(source_origin) === "[object Function]" && source_origin(e.origin) === !1)) {
-							return !1;
-						}
-						callback(e);
-					};
-				}
-				if (window['addEventListener']) {
-					window[callback ? 'addEventListener' : 'removeEventListener']('message', attached_callback, !1);
-				} else {
-					window[callback ? 'attachEvent' : 'detachEvent']('onmessage', attached_callback);
-				}
-			} else {
-				// a polling loop is started & callback is called whenever the location.hash changes
-				interval_id && clearInterval(interval_id);
-				interval_id = null;
-				if (callback) {
-					interval_id = setInterval(function () {
-						var hash = document.location.hash,
-							re = /^#?\d+&/;
-						if (hash !== last_hash && re.test(hash)) {
-							last_hash = hash;
-							callback({
-								data: hash.replace(re, '')
-							});
-						}
-					}, 100);
-				}
-			}
-		}
-	};
-}();
 //// check for another instance of namespace
 //if(typeof $mozhunt === 'undefined')
 //{
@@ -327,17 +270,93 @@ $mozhuntpostmessage = function () {
 //	}
 //}
 
-$mozhuntpostmessage.receiveMessage(function(msg){
-	msg = JSON.parse(msg);
+var XD = function () {
+	var interval_id, last_hash, cache_bust = 1,
+		attached_callback, window = this;
+	console.log(window);
+	return {
+		postMessage: function (message, target_url, target) {
+			if (!target_url) {
+				return;
+			}
+			target = target || parent; // default to parent
+			if (window['postMessage']) {
+				// the browser supports window.postMessage, so call it with a targetOrigin
+				// set appropriately, based on the target_url parameter.
+				target['postMessage'](message, target_url.replace(/([^:]+:\/\/[^\/]+).*/, '$1'));
+			} else if (target_url) {
+				// the browser does not support window.postMessage, so set the location
+				// of the target to target_url#message. A bit ugly, but it works! A cache
+				// bust parameter is added to ensure that repeat messages trigger the callback.
+				target.location = target_url.replace(/#.*$/, '') + '#' + (+new Date) + (cache_bust++) + '&' + message;
+			}
+		},
+		receiveMessage: function (callback, source_origin) {
+			// browser supports window.postMessage
+			if (window['postMessage']) {
+				// bind the callback to the actual event associated with window.postMessage
+				if (callback) {
+					attached_callback = function (e) {
+						if ((typeof source_origin === 'string' && e.origin !== source_origin) || (Object.prototype.toString.call(source_origin) === "[object Function]" && source_origin(e.origin) === !1)) {
+							return !1;
+						}
+						callback(e);
+					};
+				}
+				if (window['addEventListener']) {
+					window[callback ? 'addEventListener' : 'removeEventListener']('message', attached_callback, !1);
+				} else {
+					window[callback ? 'attachEvent' : 'detachEvent']('onmessage', attached_callback);
+				}
+			} else {
+				// a polling loop is started & callback is called whenever the location.hash changes
+				interval_id && clearInterval(interval_id);
+				interval_id = null;
+				if (callback) {
+					interval_id = setInterval(function () {
+						var hash = document.location.hash,
+							re = /^#?\d+&/;
+						if (hash !== last_hash && re.test(hash)) {
+							last_hash = hash;
+							callback({
+								data: hash.replace(re, '')
+							});
+						}
+					}, 100);
+				}
+			}
+		}
+	};
+}();
+
+$mozhunt = {
+	iframe : function(url){
+		url += '#' + encodeURIComponent(document.location.href);
+		
+		var iframe = document.createElement('iframe');
+		iframe.src = url;
+		iframe.style.width = '0';
+		iframe.style.height = '0';
+		iframe.style.visibility = 'hidden';
+		iframe.id = 'mozhuntconnection';
+		iframe.name = 'mozhuntconnection';
+		return document.body.appendChild(iframe);
+	},
+	init : function(config){
+		this.iframe('//www.mozhunt.com/token/api/verify/'+(config.tokenid)+'/'+(config.apikey));
+		try
+		{
+			this.token = document.getElementById('mozhunttoken');
+			this.token.img = document.getElementById('mozhunttokenimg');
+		}
+		catch(e)
+		{
+			return;
+		}
+	}
+};
+
+XD.receiveMessage(function(msg){
 	console.log(msg);
 	return false;
 }, 'www.mozhunt.com');
-
-$mozhunt = {};
-$mozhunt.send = function(msg)
-{
-	var parentURL = decodeURIComponent(document.location.hash.replace(/^#/, ''));
-	msg = JSON.stringify(msg);
-	$mozhuntpostmessage.postMessage(msg, parentURL, parent);
-	return false;
-};
