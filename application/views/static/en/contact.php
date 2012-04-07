2d2dcfd2dd6d729ee06e89f0a30fbc67427b9a40
William Duyck <wduyck@gmail.com>
2012-04-07 20:55:30 +0100

    more content for the contact page

--- application/views/static/en/contact.php
+++ application/views/static/en/contact.php
@@ -2,6 +2,22 @@
 		<section class="row">
 			<article class="span6">
 				<h1>Contact Us</h1>
+				<p>So its 2012 and the internet is a big place, the world is a small one...
+				but communication is still an important part of how things happen and we
+				would love to hear your feedback as well as any ideas, suggestions, even
+				your complains and questions!</p>
+				<p>There are a number of ways to contact us. First one is of course the contact
+				form on this page... or by sending us an email direct to <a href="mailto:contact@mozhunt.com">contact@mozhunt.com</a></p>
+				<p>However... being 2012 there are also these things we call social
+				networks... and guess who is on two of the biggest! Thats right... US!</p>
+				<p style="text-align:center">
+					<a href="//facebook.com/mozhunt" class="btn btn-large">
+						<img src="asset/img/facebook.png" alt="facebook icon" /> Facebook
+					</a>
+					<a href="//twitter.com/mozhunt" class="btn btn-large">
+						<img src="asset/img/twitter.png" alt="twitter icon" /> Twitter
+					</a>
+				</p>
 			</article>
 			<section class="span6">
 				<?php $this->load->view('form/contact'); ?>
