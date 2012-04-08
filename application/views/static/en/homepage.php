adf317e62a3f4d4294324b0aacde4cd316fabc9b
William Duyck <wduyck@gmail.com>
2012-04-08 16:47:29 +0100

    added new content to homepage, updated the svgs for most of the pandas (tokens), added screencast on how to add domains/tokens

--- application/views/static/en/homepage.php
+++ application/views/static/en/homepage.php
@@ -1,16 +1,23 @@
-<section class="hero-unit">
+<section class="hero-unit clearfix">
 	<h1>mozhunt, coming soon</h1>
 	<p>The one and only, cross domain treasure hunt is back, and this time
 	its all about them easter eggs! With just a few days to go we are looking
 	for those who think they have what it takes to hide easter eggs, and hide
 	them well. So... do you think you have what it takes?</p>
-	<div class="alert alert-block clearfix">
+	<!--<div class="alert alert-block clearfix">
 		<p>The site is still under development as are some areas of the game,
 		however... all the key components are now usable!<p>
 		<p>Please be patient on areas of the site that are not working yet functional, 
 		such as "<strong>Play</strong>", and report any issues that you find via
 		<a href="//www.github.com/fuzzyfox/mozhunt/issues">github</a>.</p>
-	<span class="pull-right">-- mozhunt team</span></div>
+	<span class="pull-right">-- mozhunt team</span></div>-->
+	<h2>Happy Easter!</h2>
+	<img src="asset/img/eastereggs.png" alt="mozhnunt easter eggs" class="pull-right" />
+	<p>Well... today is easter sunday, and you know what that means... It means we
+	are all off filling up on them Easter Eggs. Its not all doom and gloom though.
+	Here is a little something to look forward to. We are going to be running a belated
+	Easter Egg Hunt in just a few days. So keep your eyes peeled, and follow us on twitter
+	to stay up to date with the latest news.</p>
 	<a href="//twitter.com/mozhunt" class="btn btn-primary btn-large">Follow us on twitter</a>
 	<a href="user/join" class="btn btn-success btn-large">Sign up now!</a>
 </section>
